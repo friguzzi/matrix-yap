f488cf18915f6f5e55bc563235d48c94ccf496d9
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-10-06 17:05:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# matrix
+
+Initial port of the matrix library of Yap (matrix_yap.pl) for SWI-Prolog. This is currently not working.
---
+++ pack.pl
@@ -0,0 +1,5 @@
+name(matrix-yap).
+title('Operations with matrices, ported from Yap').
+version('1.0').
+author('Fabrizio Riguzzi', 'fabrizio.riguzzi@unife.it').
+
